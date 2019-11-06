7a256057845929d8252dac3e3f1e1dd8d9f75c90
Burd89 <50182218+Burd89@users.noreply.github.com>
2019-11-06 11:52:35 +0100

    Add files via upload

--- Test2.R
+++ Test2.R
@@ -1,46 +1,50 @@
 library(rjson)
 library(dplyr)
 library(googledrive)
+library(htmltools)
+library(plotly)
 
-drive_download("TestData.csv", type = "csv", overwrite = TRUE)
+
+#drive_download("TestData.csv", type = "csv", overwrite = TRUE)
 #expDF1 <- data.frame()
-expDF1 <- read.csv("TestData.csv")
+#expDF1 <- read.csv("TestData.csv")
 
 
-url = "https://api.tibiadata.com/v2/highscores/Faluna/Experience/Knight.json"
-expDF <- jsonlite::fromJSON(url)
-expDF <- data.frame(expDF$highscores$data)
-expDF["time"] <- Sys.time()
-expDF1 <- rbind(expDF1, expDF %>% filter(expDF$name == "Wats Ke Burd"))
+#url = "https://api.tibiadata.com/v2/highscores/Faluna/Experience/Knight.json"
+#expDF <- jsonlite::fromJSON(url)
+#expDF <- data.frame(expDF$highscores$data)
+#expDF["time"] <- Sys.time()
+#expDF1 <- rbind(expDF1, expDF %>% filter(expDF$name == "Wats Ke Burd"))
 
-url = "https://api.tibiadata.com/v2/highscores/Faluna/Experience/Druid.json"
-expDF <- jsonlite::fromJSON(url)
-expDF <- data.frame(expDF$highscores$data)
-expDF["time"] <- Sys.time()
-expDF1 <- rbind(expDF1, expDF %>% filter(expDF$name == "Po Ni Teel"))
+#url = "https://api.tibiadata.com/v2/highscores/Faluna/Experience/Druid.json"
+#expDF <- jsonlite::fromJSON(url)
+#expDF <- data.frame(expDF$highscores$data)
+#expDF["time"] <- Sys.time()
+#expDF1 <- rbind(expDF1, expDF %>% filter(expDF$name == "Po Ni Teel"))
 
-url = "https://api.tibiadata.com/v2/highscores/Faluna/Experience/Sorcerer.json"
-expDF <- jsonlite::fromJSON(url)
-expDF <- data.frame(expDF$highscores$data)
-expDF["time"] <- Sys.time()
-expDF1 <- rbind(expDF1, expDF %>% filter(expDF$name == "Don Teeltje"))
+#url = "https://api.tibiadata.com/v2/highscores/Faluna/Experience/Sorcerer.json"
+#expDF <- jsonlite::fromJSON(url)
+#expDF <- data.frame(expDF$highscores$data)
+#expDF["time"] <- Sys.time()
+#expDF1 <- rbind(expDF1, expDF %>% filter(expDF$name == "Don Teeltje"))
 
-url = "https://api.tibiadata.com/v2/highscores/Faluna/Experience/Paladin.json"
-expDF <- jsonlite::fromJSON(url)
-expDF <- data.frame(expDF$highscores$data)
-expDF["time"] <- Sys.time()
-expDF1 <- rbind(expDF1, expDF %>% filter(expDF$name == "Ari Bombari"))
+#url = "https://api.tibiadata.com/v2/highscores/Faluna/Experience/Paladin.json"
+#expDF <- jsonlite::fromJSON(url)
+#expDF <- data.frame(expDF$highscores$data)
+#expDF["time"] <- Sys.time()
+#expDF1 <- rbind(expDF1, expDF %>% filter(expDF$name == "Ari Bombari"))
 
-write.csv(expDF1, file = "TestData.csv")
+#write.csv(expDF1, file = "TestData.csv")
 
-drive_upload("TestData.csv")
+#drive_upload("TestData.csv")
 
 #test
 
 drive_download("TestData.csv", type = "csv", overwrite = TRUE)
 #expDF1 <- data.frame()
 expDF1 <- read.csv("TestData.csv")
-
+expDF1$X <- NULL
+expDF1$time <- as.Date(expDF1$time)
 vocation <- c("Knight","Sorcerer","Druid","Paladin")
 names <- c("Wats Ke Burd","Don Teeltje","Po Ni Teel","Ari Bombari")
 
@@ -50,10 +54,25 @@ for (i in 1:4) {
   url = paste("https://api.tibiadata.com/v2/highscores/Faluna/Experience/",vocation[i],".json", sep = "")
   expDF <- jsonlite::fromJSON(url)
   expDF <- data.frame(expDF$highscores$data)
-  expDF["time"] <- Sys.time()
+  expDF["time"] <- as.Date(Sys.time())
   expDF1 <- rbind(expDF1, expDF %>% filter(expDF$name == names[i]))
 }
 
 write.csv(expDF1, file = "TestData.csv")
 
 drive_upload("TestData.csv")
+
+
+# graph
+expDF3 <- expDF1 %>% filter(expDF1$name == "Ari Bombari")
+expDF3$level <- ceiling(expDF3$level/3*2)
+expDF3$points <- 50/3 * (expDF3$level ** 3 - 6 * expDF3$level ** 2 + 17 * expDF3$level - 12)
+expDF3$name <- "Exp Share Range"
+expDF3$voc <- "na"
+expDF3$rank <- 1
+expDF1 <- rbind(expDF1, expDF3)
+
+p <- plot_ly(expDF1, x = as.Date(expDF1$time), y = expDF1$points, color = expDF1$name, type = "scatter", mode = "lines")
+p
+save_html(p, file = "Testgraph.html")
+drive_upload("Testgraph.html")
